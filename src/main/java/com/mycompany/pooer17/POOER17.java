d74ad717afc0ea62d71ba9a9ac07c1fcdcdafaa8
julir <julir@DESKTOP-1TIO6PV>
2023-08-26 18:09:40 -0500

    Carga inicial1

---
+++ src/main/java/com/mycompany/pooer17/POOER17.java
@@ -0,0 +1,25 @@
+package com.mycompany.pooer17;
+import java.util.Scanner;
+/**
+ *
+ * @author JuliRuizOcampo
+ */
+public class POOER17 {
+
+    public static void main(String[] args) {
+        
+        int n, num = 1, suma = 0;
+        
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Ingrese el número natural hasta el cual desea realizar la suma: ");
+        n = lector.nextInt();
+        
+        //Procedimiento de la suma
+        while (num <=  n) {
+            suma += num; //equivalente a suma = suma + num
+            num += 1; //equivalente a num = num + 1
+        }
+        
+        System.out.println("La suma es: " + suma); 
+    }
+}
